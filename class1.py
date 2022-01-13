2900183131d7e4a6fa1a5a0118f32a72b9c72533
ShashankR123 <97449686+ShashankR123@users.noreply.github.com>
2022-01-13 08:25:13 -0500

    Update class1.py

--- class1.py
+++ class1.py
@@ -11,25 +11,25 @@ txtfld=Entry(window, text="what date do you want to predict for?", bd=5)
 txtfld.place(x=80, y=150)
 var = StringVar()
 var.set("one")
-data=("Yes", "No")
+options=("Yes", "No")
 
 lbl=Label(window, text="Doge Prediction", fg='blue', font=("Helvetica", 16))
 lbl.place(x=60, y=50)
 
 lbl=Label(window, text="Do you want to use twitter data?", fg='black', font=("Helvetica", 10))
 lbl.place(x=60, y=200)
-lb=Listbox(window, height=2, selectmode='single')
-def yesno(x, y):    
-    for num in data:
-        lb.insert(END,num)
-    lb.place(x=x, y=y)
-yesno(50, 250)
+lb1=Listbox(window, height=2, selectmode='single')
+def yesno(x, y, box):    
+    for num in options:
+        box.insert(END,num)
+    box.place(x=x, y=y)
+yesno(50, 250, lb1)
 
 lbl=Label(window, text="Do you want to use past trends?", fg='black', font=("Helvetica", 10))
 lbl.place(x=60, y=300)
 
-lb=Listbox(window, height=2, selectmode='single')
-yesno(60, 350)
+lb2=Listbox(window, height=2, selectmode='single')
+yesno(60, 350, lb2)
 
 window.title('Doge Prediction')
 window.geometry("300x800+10+20")
