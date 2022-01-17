490f4e54714526fa3c30c5777a4d9b67882dcf53
ShashankR123 <97449686+ShashankR123@users.noreply.github.com>
2022-01-16 19:25:10 -0500

    Update class1.py

--- class1.py
+++ class1.py
@@ -1,39 +1,55 @@
 from tkinter import *
-from tkinter.ttk import Combobox
 window=Tk()
 # add widgets here
-btn=Button(window, text="Get Prediction", fg='blue')
-btn.place(x=80, y=400)
 
-lbl = Label(window, text="What date do you want to predict for?", fg='black', font=("Helvetica", 10))
-lbl.place(x=60, y=100)
-txtfld = Entry(window, text="what date do you want to predict for?", bd=5)
-txtfld.place(x=80, y=150)
-var = StringVar()
-var.set("one")
 data = ("Yes", "No")
 
-lbl.place(x=60, y=50)
+# Date entry for the day of prediction
+lbl = Label(window, text="What date do you want to predict for?", fg='black', font=("Helvetica", 10))
+lbl.place(x=60, y=100)
 
+txtfld = Entry(window, text="what date do you want to predict for?", bd=5)
+txtfld.place(x=80, y=150)
+
+# option for using twitter data
 lbl = Label(window, text="Do you want to use twitter data?", fg='black', font=("Helvetica", 10))
 lbl.place(x=60, y=200)
+#This is yes/no option box for twitter data
 lb = Listbox(window, height=2, selectmode='single')
+for num in data:
+  lb.insert(END,num)
+lb.place(x=80, y=250)
 
-
-def yesno(x, y):    
-    for num in data:
-        lb.insert(END,num)
-    lb.place(x=x, y=y)
-
-
-yesno(50, 250)
-
+# option for using data on past trends
 lbl = Label(window, text="Do you want to use past trends?", fg='black', font=("Helvetica", 10))
 lbl.place(x=60, y=300)
+#this is the yes/no option box for using past trends
+lb2 = Listbox(window, height=2, selectmode='single')
+for num in data:
+  lb2.insert(END,num)
+lb2.place(x=80, y=350)
 
-lb = Listbox(window, height=2, selectmode='single')
-yesno(60, 350)
+#how much past data to use
+lbl = Label(window, text="How much past data do you want to use?", fg='black', font=("Helvetica", 10))
+lbl.place(x=60, y=400)
+  # enter how much data you want to use. Days, weeks, months, or years?
+txtfld = Entry(window, text="How much past data do you want to use?", bd=5)
+txtfld.place(x=80, y=450)
 
-window.title('Doge Prediction')
-window.geometry("300x800+10+20")
+#Option for using data of other crypto-currencies
+lbl = Label(window, text="Do you want to use data from other crypto currencies?", fg='black', font=("Helvetica", 10))
+lbl.place(x=60, y=500)
+#This is yes/no option box for using data form other crypto currencies
+lb3 = Listbox(window, height=2, selectmode='single')
+for num in data:
+  lb3.insert(END,num)
+lb3.place(x=80, y=550)
+
+# button you click to get results
+btn=Button(window, text="Get Prediction", fg='blue')
+btn.place(x=80, y=600)
+
+
+window.title('Dogecoin Price Prediction')
+window.geometry("500x800+10+20")
 window.mainloop()
